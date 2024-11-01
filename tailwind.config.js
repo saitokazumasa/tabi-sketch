93c332a07f4327265c8c1f669af2c33813a95894
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2024-11-01 14:46:00 +0900

    colorsの設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "label" : "#555",
+        "frame" : "#D9D9D9",
+        "sub-button" : "#5F8565",
+        "interactive" : "#8DC48F",
+        "link" : "#8DA3C4",
+        "danger" : "#F45555"
+      },
+    },
   },
   plugins: [],
 }
