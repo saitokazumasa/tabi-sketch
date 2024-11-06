bf5b9e5d22fe517d769ccc2ecc6da08c3994c64b
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2024-11-06 14:56:16 +0900

    インデント4に変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {
-      colors: {
-        "label" : "#555",
-        "frame" : "#D9D9D9",
-        "sub-button" : "#5F8565",
-        "interactive" : "#8DC48F",
-        "link" : "#8DA3C4",
-        "danger" : "#F45555"
-      },
-      fontFamily: {
-        sans: ["M PLUS 1p", "sans-serif"],
-      }
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                "label" : "#555",
+                "frame" : "#D9D9D9",
+                "sub-button" : "#5F8565",
+                "interactive" : "#8DC48F",
+                "link" : "#8DA3C4",
+                "danger" : "#F45555"
+            },
+            fontFamily: {
+                sans: ["M PLUS 1p", "sans-serif"],
+            }
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
 
