bdd20811cee37ea0dd49ec92c99590bb01b3ca3c
t-dounoma <t.dounomae.sys21@morijyobi.ac.jp>
2024-11-08 10:20:35 +0900

    [add] ログインページの作成
    - ボタンとインプットボックスが完成したため、クラス指定済み
    - head,header,footerについてはcommit時点で未完成だったため、完成ししだい追記
    - エラーメッセージが要件になかったがあったほうがいいと思ったため相談済み

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-        extend: {
-            colors: {
-                "label" : "#555",
-                "frame" : "#D9D9D9",
-                "sub-button" : "#5F8565",
-                "interactive" : "#8DC48F",
-                "link" : "#8DA3C4",
-                "danger" : "#F45555"
-            },
-            fontFamily: {
-                sans: ["M PLUS 1p", "sans-serif"],
-            }
-        },
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "default" : "#1E1E1E",
+        "label" : "#555",
+        "frame" : "#D9D9D9",
+        "sub-button" : "#5F8565",
+        "interactive" : "#8DC48F",
+        "link" : "#8DA3C4",
+        "link-hover" : "#6F8ECE",
+        "danger" : "#F45555",
+      },
+      fontFamily: {
+        sans: ["M PLUS 1p", "sans-serif"],
+      }
     },
-    plugins: [],
+  },
+  plugins: [],
 }
 
