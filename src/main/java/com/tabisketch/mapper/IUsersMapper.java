c1f9b91703b1d23df7ab3c6758277042e3553dd3
saito-kazumasa <kotobukisyo@gmail.com>
2024-11-14 12:44:12 +0900

    LoginUserDetailsService を追加 それに伴ってIUsersMapperを簡易的に作成

---
+++ src/main/java/com/tabisketch/mapper/IUsersMapper.java
@@ -0,0 +1,11 @@
+package com.tabisketch.mapper;
+
+import com.tabisketch.bean.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface IUsersMapper {
+    @Select("SELECT * FROM users WHERE mail = #{mail}")
+    User selectByMail(final String mail);
+}
