f0e03b303810d558811d0c243e81dd694e2827d0
t-dounoma <t.dounomae.sys21@morijyobi.ac.jp>
2024-11-18 09:58:30 +0900

    [fix] 作業のロールバック

--- src/main/resources/static/js/header.js
+++
@@ -1,7 +0,0 @@
-const tham = document.querySelector(".tham");
-const mobileMenu = document.getElementById("mobile-menu");
-
-tham.addEventListener('click', () => {
-    tham.classList.toggle('tham-active');
-    mobileMenu.classList.toggle('hidden');
-});
