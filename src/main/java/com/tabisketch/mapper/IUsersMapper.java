a7e51929dbad49224e8990ef40a9f4c9b8655c8c
saito-kazumasa <kotobukisyo@gmail.com>
2024-11-25 12:26:39 +0900

    UsersMapper に insert を実装

--- src/main/java/com/tabisketch/mapper/IUsersMapper.java
+++ src/main/java/com/tabisketch/mapper/IUsersMapper.java
@@ -1,11 +1,17 @@
 package com.tabisketch.mapper;
 
 import com.tabisketch.bean.entity.User;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface IUsersMapper {
+    @Insert("INSERT INTO users (mail, password) VALUES (#{mail}, #{password})")
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    int insert(final User user);
+
     @Select("SELECT * FROM users WHERE mail = #{mail}")
     User selectByMail(final String mail);
 }
