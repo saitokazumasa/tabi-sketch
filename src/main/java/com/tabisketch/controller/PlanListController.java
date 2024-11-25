5120bff9c50858188c234cc5b529549c914e077b
imanoari <101164506+imanoari@users.noreply.github.com>
2024-11-25 12:04:13 +0900

    @Controllerの追加

--- src/main/java/com/tabisketch/controller/PlanListController.java
+++ src/main/java/com/tabisketch/controller/PlanListController.java
@@ -1,9 +1,11 @@
 package com.tabisketch.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("plan/list")
+@Controller
+@RequestMapping("/plan/list")
 public class PlanListController {
     @GetMapping()
     public String planList() {
