88e1e3de2e044eeb49accce7beaac321c7be97d1
BruinJr <91596528+BruinJr@users.noreply.github.com>
2024-11-25 12:20:47 +0900

    spacingを追加
    旅行一覧画面のwidthを追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
     content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
     theme: {
+        spacing: {
+            'list-box': '29.1875rem',
+        },
         extend: {
             colors: {
                 "label" : "#555",
