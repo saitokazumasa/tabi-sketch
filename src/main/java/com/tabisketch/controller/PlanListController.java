700633bc9e7a4752ba28f20e3d9c53b4323d1e6b
imanoari <101164506+imanoari@users.noreply.github.com>
2024-11-25 10:01:24 +0900

    PlanListControllerの作成

--- src/main/java/com/tabisketch/controller/PlanListController.java
+++ src/main/java/com/tabisketch/controller/PlanListController.java
@@ -3,11 +3,11 @@ package com.tabisketch.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("")
+@RequestMapping("plan/list")
 public class PlanListController {
     @GetMapping()
     public String planList() {
-        return "";
+        return "plan/list";
     }
 
 }
