fea7071857a221b2d8d350334179f1ff9af94392
imanoari <101164506+imanoari@users.noreply.github.com>
2024-11-28 13:52:12 +0900

    テストデータ作成

---
+++ src/test/java/com/tabisketch/controller/EditUserControllerTest.java
@@ -0,0 +1,23 @@
+package com.tabisketch.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest(EditUserController.class)
+class EditUserControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @WithMockUser
+    public void getが動作するか() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/user/edit"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("user/edit/edit"));
+    }
+}
