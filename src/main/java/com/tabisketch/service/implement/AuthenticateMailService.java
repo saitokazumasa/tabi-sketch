5a1685de11aa46d9ddbe621f3dcee3231956ea56
saito-kazumasa <kotobukisyo@gmail.com>
2024-11-29 12:44:00 +0900

    Controllerと結合

--- src/main/java/com/tabisketch/service/implement/AuthenticateMailService.java
+++ src/main/java/com/tabisketch/service/implement/AuthenticateMailService.java
@@ -1,28 +1,35 @@
 package com.tabisketch.service.implement;
 
 import com.tabisketch.mapper.IMailAuthenticationTokensMapper;
+import com.tabisketch.mapper.IUsersMapper;
 import com.tabisketch.service.IAuthenticateMailService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
 @Service
 public class AuthenticateMailService implements IAuthenticateMailService {
     private final IMailAuthenticationTokensMapper mailAuthenticationTokensMapper;
+    private final IUsersMapper usersMapper;
 
-    public AuthenticateMailService(final IMailAuthenticationTokensMapper mailAuthenticationTokensMapper) {
+    public AuthenticateMailService(
+            final IMailAuthenticationTokensMapper mailAuthenticationTokensMapper,
+            final IUsersMapper usersMapper
+    ) {
         this.mailAuthenticationTokensMapper = mailAuthenticationTokensMapper;
+        this.usersMapper = usersMapper;
     }
 
     @Override
+    @Transactional
     public boolean execute(final String token) {
         final var tokenUUID = UUID.fromString(token);
         final var mailAuth = this.mailAuthenticationTokensMapper.selectByToken(tokenUUID);
 
         if (mailAuth == null) return false;
 
-        // TODO: メール認証完了
-
+        this.usersMapper.updateMailVerified(mailAuth.getUserId(), true);
         return true;
     }
 }
--- src/test/java/com/tabisketch/service/AuthenticateMailServiceTest.java
+++ src/test/java/com/tabisketch/service/AuthenticateMailServiceTest.java
@@ -2,6 +2,7 @@ package com.tabisketch.service;
 
 import com.tabisketch.bean.entity.MailAuthenticationToken;
 import com.tabisketch.mapper.IMailAuthenticationTokensMapper;
+import com.tabisketch.mapper.IUsersMapper;
 import com.tabisketch.service.implement.AuthenticateMailService;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -16,14 +17,17 @@ import static org.mockito.Mockito.when;
 public class AuthenticateMailServiceTest {
     @MockBean
     private IMailAuthenticationTokensMapper mailAuthenticationTokensMapper;
+    @MockBean
+    private IUsersMapper usersMapper;
 
     @ParameterizedTest
     @ValueSource(strings = {"a2e69add-9d95-4cf1-a59b-cedbb95dcd6b"})
     public void 動作するか(final String token) {
         final var tokenUUID = UUID.fromString(token);
-        when(mailAuthenticationTokensMapper.selectByToken(tokenUUID)).thenReturn(new MailAuthenticationToken());
+        when(this.mailAuthenticationTokensMapper.selectByToken(tokenUUID)).thenReturn(new MailAuthenticationToken());
 
-        final var authenticateMailService = new AuthenticateMailService(mailAuthenticationTokensMapper);
+        final var authenticateMailService =
+                new AuthenticateMailService(this.mailAuthenticationTokensMapper, this.usersMapper);
         assert authenticateMailService.execute(token);
     }
 }
