5f1e42961bd2e645de7cd92fbab9b1283c968c22
imanoari <101164506+imanoari@users.noreply.github.com>
2024-11-29 12:11:26 +0900

    テストデータ修正

--- src/test/java/com/tabisketch/controller/EditUserControllerTest.java
+++ src/test/java/com/tabisketch/controller/EditUserControllerTest.java
@@ -9,7 +9,8 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @WebMvcTest(EditUserController.class)
-class EditUserControllerTest {
+public class EditUserControllerTest {
+
     @Autowired
     private MockMvc mockMvc;
 
@@ -18,6 +19,7 @@ class EditUserControllerTest {
     public void getが動作するか() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/user/edit"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.view().name("user/edit/index"));
+                .andExpect(MockMvcResultMatchers.view().name("user/edit/index"))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("item"));
     }
 }
