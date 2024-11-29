3e3d455f068daff4c31f0e3e23dc7bba7fe99c52
saito-kazumasa <kotobukisyo@gmail.com>
2024-11-29 10:31:48 +0900

    メール認証マッパーを本実装

--- src/main/java/com/tabisketch/mapper/IMailAuthenticationTokensMapper.java
+++ src/main/java/com/tabisketch/mapper/IMailAuthenticationTokensMapper.java
@@ -3,9 +3,12 @@ package com.tabisketch.mapper;
 import com.tabisketch.bean.entity.MailAuthenticationToken;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 
 @Mapper
 public interface IMailAuthenticationTokensMapper {
-    @Insert("")
+    @Insert("INSERT INTO mail_authentication_tokens (token, user_id) " +
+            "VALUES (#{token}, #{userId})")
+    @Options(useGeneratedKeys = true, keyProperty = "id")
     int insert(final MailAuthenticationToken mailAuthenticationToken);
 }
