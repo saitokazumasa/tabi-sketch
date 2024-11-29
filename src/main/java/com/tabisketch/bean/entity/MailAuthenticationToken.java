83428d747d78e683772fd12b2460462bba75e659
saito-kazumasa <kotobukisyo@gmail.com>
2024-11-29 10:21:27 +0900

    メール認証エンティティを作成

---
+++ src/main/java/com/tabisketch/bean/entity/MailAuthenticationToken.java
@@ -0,0 +1,17 @@
+package com.tabisketch.bean.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MailAuthenticationToken {
+    private int id;
+    private String token;
+    private int userId;
+    private LocalDateTime createdAt;
+}
