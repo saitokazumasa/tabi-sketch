d6896fc8fa63057f3fa906605644dc686df5baef
imanoari <101164506+imanoari@users.noreply.github.com>
2024-11-29 12:11:45 +0900

    Controller修正

--- src/main/java/com/tabisketch/controller/EditUserController.java
+++ src/main/java/com/tabisketch/controller/EditUserController.java
@@ -1,14 +1,20 @@
 package com.tabisketch.controller;
 
+import com.tabisketch.bean.entity.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/user/edit")
 public class EditUserController {
+
     @GetMapping
-    public String get() {
+    public String get(Model model) {
+        User item = new User();
+        item.setMail("user@example.com");
+        model.addAttribute("item", item);
         return "user/edit/index";
     }
 }
