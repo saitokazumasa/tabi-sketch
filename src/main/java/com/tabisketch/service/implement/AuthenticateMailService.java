7a0f3d763743eb4038cebe91e6d92af89c91d066
saito-kazumasa <kotobukisyo@gmail.com>
2024-11-29 12:29:12 +0900

    メール認証サービスを実装

--- src/main/java/com/tabisketch/service/implement/AuthenticateMailService.java
+++ src/main/java/com/tabisketch/service/implement/AuthenticateMailService.java
@@ -1,12 +1,28 @@
 package com.tabisketch.service.implement;
 
+import com.tabisketch.mapper.IMailAuthenticationTokensMapper;
 import com.tabisketch.service.IAuthenticateMailService;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 public class AuthenticateMailService implements IAuthenticateMailService {
+    private final IMailAuthenticationTokensMapper mailAuthenticationTokensMapper;
+
+    public AuthenticateMailService(final IMailAuthenticationTokensMapper mailAuthenticationTokensMapper) {
+        this.mailAuthenticationTokensMapper = mailAuthenticationTokensMapper;
+    }
+
     @Override
-    public boolean execute(String token) {
-        return false;
+    public boolean execute(final String token) {
+        final var tokenUUID = UUID.fromString(token);
+        final var mailAuth = this.mailAuthenticationTokensMapper.selectByToken(tokenUUID);
+
+        if (mailAuth == null) return false;
+
+        // TODO: メール認証完了
+
+        return true;
     }
 }
--- src/test/java/com/tabisketch/service/AuthenticateMailServiceTest.java
+++ src/test/java/com/tabisketch/service/AuthenticateMailServiceTest.java
@@ -1,11 +1,16 @@
 package com.tabisketch.service;
 
+import com.tabisketch.bean.entity.MailAuthenticationToken;
 import com.tabisketch.mapper.IMailAuthenticationTokensMapper;
 import com.tabisketch.service.implement.AuthenticateMailService;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.UUID;
+
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 public class AuthenticateMailServiceTest {
@@ -15,7 +20,10 @@ public class AuthenticateMailServiceTest {
     @ParameterizedTest
     @ValueSource(strings = {"a2e69add-9d95-4cf1-a59b-cedbb95dcd6b"})
     public void 動作するか(final String token) {
-        final var authenticateMailService = new AuthenticateMailService();
+        final var tokenUUID = UUID.fromString(token);
+        when(mailAuthenticationTokensMapper.selectByToken(tokenUUID)).thenReturn(new MailAuthenticationToken());
+
+        final var authenticateMailService = new AuthenticateMailService(mailAuthenticationTokensMapper);
         assert authenticateMailService.execute(token);
     }
 }
