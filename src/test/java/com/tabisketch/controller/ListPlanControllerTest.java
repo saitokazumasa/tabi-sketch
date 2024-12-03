4884e9322922ff5908bc7615c1f831cdd404d8ff
saito-kazumasa <kotobukisyo@gmail.com>
2024-12-03 10:49:36 +0900

    ListPlanControllerを本実装

--- src/test/java/com/tabisketch/controller/ListPlanControllerTest.java
+++ src/test/java/com/tabisketch/controller/ListPlanControllerTest.java
@@ -1,23 +1,45 @@
 package com.tabisketch.controller;
 
+import com.tabisketch.bean.entity.Plan;
+import com.tabisketch.service.IListPlanService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.when;
+
 @WebMvcTest(ListPlanController.class)
 public class ListPlanControllerTest {
     @Autowired
     private MockMvc mockMvc;
+    @MockBean
+    private IListPlanService listPlanService; // IDで使用している
 
     @Test
-    @WithMockUser
+    @WithMockUser(username = "sample@example.com")
     public void getが動作するか() throws Exception {
+        final String mail = getCurrentMail();
+        when(listPlanService.execute(mail)).thenReturn(new ArrayList<Plan>());
+
         mockMvc.perform(MockMvcRequestBuilders.get("/plan/list"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.model().attributeExists("planList"))
+                .andExpect(MockMvcResultMatchers.model().attribute("planList", new ArrayList<Plan>()))
                 .andExpect(MockMvcResultMatchers.view().name("plan/list"));
+    }
+
+    private String getCurrentMail() {
+        return SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getName();
     }
 }
