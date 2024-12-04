011cb9224d8474d0b14a9bd62f1e7a9d16f5abf8
imanoari <101164506+imanoari@users.noreply.github.com>
2024-12-04 12:19:14 +0900

    Serviceいらない所修正

--- src/main/java/com/tabisketch/service/implement/EditUserService.java
+++ src/main/java/com/tabisketch/service/implement/EditUserService.java
@@ -12,19 +12,12 @@ public class EditUserService {
         this.usersMapper = usersMapper;
     }
 
-    // 新規ユーザーを登録
-    public int createUser(final User user) {
-        return usersMapper.insert(user);
-    }
-
-    // ユーザーをメールアドレスで取得
     public User getUserByMail(final String mail) {
         return usersMapper.selectByMail(mail);
     }
-
-    // ユーザーのメール認証状態を更新
+    
     public boolean updateUserMailVerified(final int id, final boolean isMailVerified) {
         int rowsUpdated = usersMapper.updateMailVerified(id, isMailVerified);
-        return rowsUpdated > 0; // 1以上の更新があれば成功と判断
+        return rowsUpdated > 0;
     }
 }
