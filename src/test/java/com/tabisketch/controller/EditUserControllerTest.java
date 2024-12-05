84d8582f2cf7c4e4cd9994dd0a7c14148b2fa03b
imanoari <101164506+imanoari@users.noreply.github.com>
2024-12-05 11:48:49 +0900

    EditUserControllerTestの作成

--- src/test/java/com/tabisketch/controller/EditUserControllerTest.java
+++ src/test/java/com/tabisketch/controller/EditUserControllerTest.java
@@ -1,8 +1,10 @@
 package com.tabisketch.controller;
 
+import com.tabisketch.mapper.IUsersMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -10,15 +12,23 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @WebMvcTest(EditUserController.class)
 public class EditUserControllerTest {
+    @MockBean
+    private IUsersMapper usersMapper;
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    @WithMockUser
+    @WithMockUser(username = "sample@example.com")
     public void getが動作するか() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/user/edit"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.view().name("user/edit/index"))
-                .andExpect(MockMvcResultMatchers.model().attributeExists("item"));
+                .andExpect(MockMvcResultMatchers.model().attributeExists("user"));
+    }
+    @Test
+    @WithMockUser(username = "sample@example.com")
+    public void viewが正しく返るか() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/user/edit"))
+                .andExpect(MockMvcResultMatchers.view().name("user/edit/index"));
     }
 }
