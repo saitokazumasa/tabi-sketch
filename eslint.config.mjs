65e7a63b47611382c7172bd3c9a2c2a342ba2ea1
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2024-12-05 11:23:10 +0900

    グローバル変数の追加

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,7 @@ export default [
             globals: {
                 ...globals.browser,
                 google: "readonly", // google をグローバル変数として登録
+                Modal: "readonly" // Modal をグローバル変数として登録
             },
         },
         files: ["src/**/*.js"], // 適用ファイル
@@ -24,13 +25,12 @@ export default [
             "indent": ["error", 4], // インデントをスペース4つに統一
             "no-multiple-empty-lines": ["error", { "max": 1 }], // 複数行の空行を禁止
             "eol-last": ["error", "always"], // ファイルの末尾に改行を強制
+            "no-trailing-spaces": "error", // 行末の余分なスペースを禁止
+            "space-before-function-paren": ["error", "never"], // 関数の括弧前にスペースを禁止
 
             "prefer-const": "error", // 再代入しない変数は const を強制
             "arrow-spacing": ["error", { "before": true, "after": true }], // アロー関数の前後にスペースを強制
             "no-var": "error", // var を禁止 (let または const を使用)
-
-            "no-trailing-spaces": "error", // 行末の余分なスペースを禁止
-            "space-before-function-paren": ["error", "never"], // 関数の括弧前にスペースを禁止
         },
     },
     pluginJs.configs.recommended, // 推奨設定
