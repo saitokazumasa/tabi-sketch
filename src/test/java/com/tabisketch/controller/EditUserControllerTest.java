e2a15b81b29b38b257f5706a20019fc8a5756a79
imanoari <101164506+imanoari@users.noreply.github.com>
2024-12-06 10:25:22 +0900

    テストコードの修正

--- src/test/java/com/tabisketch/controller/EditUserControllerTest.java
+++ src/test/java/com/tabisketch/controller/EditUserControllerTest.java
@@ -1,34 +1,31 @@
 package com.tabisketch.controller;
 
-import com.tabisketch.mapper.IUsersMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @WebMvcTest(EditUserController.class)
 public class EditUserControllerTest {
-    @MockBean
-    private IUsersMapper usersMapper;
     @Autowired
     private MockMvc mockMvc;
 
-    @Test
-    @WithMockUser(username = "sample@example.com")
-    public void getが動作するか() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/user/edit"))
+    @ParameterizedTest
+    @ValueSource(strings = {"sample2@example.com"})
+    @WithMockUser
+    public void getが動作するか(final String mail) throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders
+                        .get("/user/edit")
+                        .flashAttr("mail", mail)
+                        .with(SecurityMockMvcRequestPostProcessors.csrf()))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.view().name("user/edit/index"))
                 .andExpect(MockMvcResultMatchers.model().attributeExists("user"));
-    }
-    @Test
-    @WithMockUser(username = "sample@example.com")
-    public void viewが正しく返るか() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/user/edit"))
-                .andExpect(MockMvcResultMatchers.view().name("user/edit/index"));
     }
 }
