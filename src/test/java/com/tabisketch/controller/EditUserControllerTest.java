5191e8dd48751ed50f31e968917c153ac1442b62
imanoari <101164506+imanoari@users.noreply.github.com>
2024-12-08 01:20:13 +0900

    attributeの値確認の部分を修正

--- src/test/java/com/tabisketch/controller/EditUserControllerTest.java
+++ src/test/java/com/tabisketch/controller/EditUserControllerTest.java
@@ -3,6 +3,7 @@ package com.tabisketch.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
@@ -16,11 +17,18 @@ public class EditUserControllerTest {
     @Test
     @WithMockUser(username = "sample@example.com")
     public void getが動作するか() throws Exception {
+        final String mail = getCurrentMail();
         this.mockMvc.perform(MockMvcRequestBuilders
-                        .get("/user/edit")
-                        .with(SecurityMockMvcRequestPostProcessors.csrf()))
+                        .get("/user/edit"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.model().attributeExists("user","sample@example.com"))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("mail"))
+                .andExpect(MockMvcResultMatchers.model().attribute("mail", mail))
                 .andExpect(MockMvcResultMatchers.view().name("user/edit/index"));
+    }
+    private String getCurrentMail() {
+        return SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getName();
     }
 }
