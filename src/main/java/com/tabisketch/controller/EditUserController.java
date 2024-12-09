2764a1cd2754274356601539d03ce0410beae101
imanoari <101164506+imanoari@users.noreply.github.com>
2024-12-09 09:43:11 +0900

    Controllerの修正

--- src/main/java/com/tabisketch/controller/EditUserController.java
+++ src/main/java/com/tabisketch/controller/EditUserController.java
@@ -1,9 +1,6 @@
 package com.tabisketch.controller;
 
-import com.tabisketch.bean.entity.User;
-import com.tabisketch.service.implement.EditUserService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,21 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/user/edit")
 public class EditUserController {
-
-    private final EditUserService editUserService;
-
-    public EditUserController(final EditUserService editUserService) {
-        this.editUserService = editUserService;
-    }
-
-    @GetMapping
-    public String get(
-            final @AuthenticationPrincipal UserDetails userDetails,
-            final Model model
-    ) {
-        String email = userDetails.getUsername();
-        User user = editUserService.getUserByMail(email);
-        model.addAttribute("item", user);
+    public String get(@AuthenticationPrincipal(expression = "username") String mail, Model model) {
+        model.addAttribute("mail", mail);
         return "user/edit/index";
     }
 }
