a4fb566e622f8de0d11836c989ca509ffd0f8f0e
t-dounoma <t.dounomae.sys21@morijyobi.ac.jp>
2024-12-09 11:25:56 +0900

    [fix]ファイルのクラス化(map.js)

--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -1,24 +1,51 @@
-function displayMap(elementId) {
-    new google.maps.Map(document.getElementById(elementId), {
-        // TODO:バック側で指定位置のポイントが取得できれば、その位置を初期位置として設定する
-        center: {lat: 35.675682101601765, lng: 139.66842469787593},
-        zoom: 12,
-    });
+class MapManager {
+    constructor(options = {}) {
+        this.defaultLocation = options.defaultLocation || {
+            // TODO:適当な初期値を指定しているため、開始地点>目的地点>終了地点の優先度でPlaceの緯度経度を取得するように修正する
+            lat: 35.675682101601765,
+            lng: 139.66842469787593
+        };
+        this.defaultZoom = options.defaultZoom || 12;
+        this.map = null;
+        this.popup = document.getElementById('popup');
+    }
+
+    displayMap(elementId) {
+        const mapElement = document.getElementById(elementId);
+        if (!mapElement) return;
+
+        this.map = new google.maps.Map(mapElement, {
+            center: this.defaultLocation,
+            zoom: this.defaultZoom
+        });
+    }
+    openPopup() {
+        if (this.popup) {
+            this.popup.style.display = 'flex';
+            this.displayMap('sp-map');
+        }
+    }
+
+    closePopup() {
+        if (this.popup) {
+            this.popup.style.display = 'none';
+        }
+    }
+    initMap() {
+        this.displayMap('map');
+    }
 }
 
+// グローバルインスタンスの作成
+const mapManager = new MapManager();
+
+// イベントリスナーの設定
+window.onload = () => mapManager.initMap();
+
 function openPopup() {
-    const popup = document.getElementById('popup');
-    popup.style.display = 'flex';
-    displayMap('sp-map');
+    mapManager.openPopup();
 }
 
 function closePopup() {
-    const popup = document.getElementById('popup');
-    popup.style.display = 'none';
+    mapManager.closePopup();
 }
-
-function initMap() {
-    displayMap('map');
-}
-
-window.onload = initMap;
