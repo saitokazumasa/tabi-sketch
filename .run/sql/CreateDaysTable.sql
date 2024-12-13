783ab5608a5d877798e4892b146031c0ea1d734f
saito-kazumasa <kotobukisyo@gmail.com>
2024-12-13 11:50:11 +0900

    DaysMapperを作成

--- .run/sql/CreateDaysTable.sql
+++ .run/sql/CreateDaysTable.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS days
 (
-    id                         SERIAL PRIMARY KEY,
-    day                        INT     NOT NULL,
-    plan_id                    INT     NOT NULL,
-    walk_threshold             INT,
-    prefer_transportation_list INT     NOT NULL,
-    is_use_toll_road           BOOLEAN NOT NULL,
-    is_use_ferry               BOOLEAN NOT NULL,
+    id                                SERIAL PRIMARY KEY,
+    day                               INT        NOT NULL,
+    plan_id                           INT        NOT NULL,
+    walk_threshold                    INT,
+    prefer_transportation_list_binary VARCHAR(4) NOT NULL,
+    is_use_toll_road                  BOOLEAN    NOT NULL DEFAULT TRUE,
+    is_use_ferry                      BOOLEAN    NOT NULL DEFAULT TRUE,
     FOREIGN KEY (plan_id) REFERENCES plans (id)
 );
