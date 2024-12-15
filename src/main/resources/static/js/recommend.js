8c3109f37388d847e820f0c9dcd640ded20080be
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2024-12-15 15:17:46 +0900

    todoコメントを記載

--- src/main/resources/static/js/recommend.js
+++ src/main/resources/static/js/recommend.js
@@ -8,10 +8,13 @@ class RecommendPlace {
         // submit送信したformを取得
         const formKey = e.target.id;
         const formNum = Number(formKey.replace('recommendForm', ''));
-        // todo: sessionに値を追加する (modal.jsのSessionStorageListのような形で実装)
 
+        // todo: sessionに値を追加 (modal.jsのSessionStorageListのplaceに入れる)
+        // todo: 今表示されている「目的地を追加する」のvalueをsessionから入れる（PR#194 modal.js setPlaceFormValue のような形）
+        // todo: 今表示されている「目的地を追加する」の表示内容変更（PR#194 modal.js changePlaceDisplay のような形）
+        // todo: 新規フラグメントの呼び出し（PR#194 modal.js placesFormSubmit のような形）
 
-        this.#hideDisplay(formNum);
+        this.#hideDisplay(formNum); // 追加したおすすめ目的地の表示を隠す
     }
 
     /**
