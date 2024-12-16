a17ddbe0ab88133e53bc672f5b898d73a2367170
saito-kazumasa <kotobukisyo@gmail.com>
2024-12-16 14:33:26 +0900

    DaysMapperをリファクタ

--- src/main/java/com/tabisketch/mapper/IDaysMapper.java
+++ src/main/java/com/tabisketch/mapper/IDaysMapper.java
@@ -13,7 +13,7 @@ public interface IDaysMapper {
     @Insert("INSERT INTO days (day, plan_id, walk_threshold, prefer_transportation_list_binary) " +
             "VALUES (#{day}, #{planId}, #{walkThreshold}, #{preferTransportationListBinary})")
     @Options(useGeneratedKeys = true, keyProperty = "id")
-    int insert(final Day day);
+    void insert(final Day day);
 
     @Select("SELECT * FROM days WHERE plan_id = #{planId}")
     List<Day> selectByPlanId(final int planId);
--- src/test/java/com/tabisketch/mapper/DaysMapperTest.java
+++ src/test/java/com/tabisketch/mapper/DaysMapperTest.java
@@ -1,14 +1,11 @@
 package com.tabisketch.mapper;
 
 import com.tabisketch.bean.entity.Day;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.test.context.jdbc.Sql;
-
-import java.util.stream.Stream;
 
 @MybatisTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -16,38 +13,35 @@ public class DaysMapperTest {
     @Autowired
     private IDaysMapper daysMapper;
 
-    @ParameterizedTest
-    @MethodSource("sampleDay")
+    @Test
     @Sql({
             "classpath:/sql/CreateUser.sql",
             "classpath:/sql/CreatePlan.sql",
     })
-    public void INSERTできるか(final Day day) {
-        final var result = this.daysMapper.insert(day);
-        assert result == 1;
+    public void testInsert() {
+        final var day = new Day(
+                -1,
+                1,
+                1,
+                0,
+                "0000",
+                true,
+                true
+        );
+        this.daysMapper.insert(day);
         assert day.getId() != -1;
     }
 
-    @ParameterizedTest
-    @MethodSource("samplePlanId")
+    @Test
     @Sql({
             "classpath:/sql/CreateUser.sql",
             "classpath:/sql/CreatePlan.sql",
             "classpath:/sql/CreateDay.sql",
     })
-    public void SELECTできるか(final int planId) {
+    public void testSelect() {
+        final int planId = 1;
         final var dayList = this.daysMapper.selectByPlanId(planId);
         assert dayList != null;
         assert !dayList.isEmpty();
-    }
-
-    private static Stream<Day> sampleDay() {
-        final var day = Day.generate(1, 1, 0, "0000");
-        return Stream.of(day);
-    }
-
-    private static Stream<Integer> samplePlanId() {
-        final var planId = 1;
-        return Stream.of(planId);
     }
 }
