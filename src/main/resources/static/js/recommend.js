08dcb5498d16cf9e77c04a152e62f9aa708f1e71
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2024-12-20 12:13:29 +0900

    form番号からbuttonの項番に

--- src/main/resources/static/js/recommend.js
+++ src/main/resources/static/js/recommend.js
@@ -7,25 +7,25 @@ class RecommendPlace {
 
         // submit送信したformを取得
         const formKey = e.target.id;
-        const formNum = Number(formKey.replace('recommendForm', ''));
+        const formNum = Number(formKey.replace('recommendSubmit', ''));
 
         // 「目的地を追加する」のform取得
-        const modalForm = new ModalForm();
-        const placeFormList = modalForm.placeFormElements;
-        const placeFormKey = placeFormList[placeFormList.length - 1].id;
-        const placeFormNum = Number(placeFormKey.replace('placeForm', ''));
+        const modalSubmitButton = new ModalSubmitButton();
+        const placeBtnList = modalSubmitButton.placeBtnElement;
+        const placeBtnKey = placeBtnList[placeBtnList.length - 1].id;
+        const placeBtnNum = Number(placeBtnKey.replace('placesSubmit', ''));
 
         // sessionに値を追加 (modal.jsのSessionStorageListのplaceに入れる)
-        sessionStorageList.setRecommendPlace(placeFormNum, formNum);
+        sessionStorageList.setRecommendPlace(placeBtnNum, formNum);
 
         // 今表示されている「目的地を追加する」のvalueをsessionから入れる
-        this.#setPlaceFormValue(placeFormNum);
+        this.#setPlaceFormValue(placeBtnNum);
 
         // 今表示されている「目的地を追加する」の表示内容変更
-        modal.changePlaceDisplay(placeFormNum);
+        modal.changePlaceDisplay(placeBtnNum);
 
         // 新規フラグメントの呼び出し
-        await modalForm.newAddFragment();
+        await modalSubmitButton.newAddFragment();
 
         this.#hideModal(formNum); // 現在開かれてるmodalを閉じる
         this.#hideDisplay(formNum); // 追加したおすすめ目的地の表示を隠す
@@ -52,29 +52,29 @@ class RecommendPlace {
 
     /**
      * sessionから値を取得してformのvalueに入れる
-     * @param placeFormNum 「目的地」formの項番
+     * @param placeBtnNum 「目的地」formの項番
      */
-    #setPlaceFormValue(placeFormNum) {
-        const data = sessionStorageList.getPlacesData(placeFormNum-1);
+    #setPlaceFormValue(placeBtnNum) {
+        const data = sessionStorageList.getPlacesData(placeBtnNum-1);
 
-        document.getElementById(`placeId${placeFormNum}`).value = data.placeId;
-        document.getElementById(`placeLat${placeFormNum}`).value = data.lat;
-        document.getElementById(`placeLng${placeFormNum}`).value = data.lng;
-        document.getElementById(`place${placeFormNum}`).value = data.name;
+        document.getElementById(`placeId${placeBtnNum}`).value = data.placeId;
+        document.getElementById(`placeLat${placeBtnNum}`).value = data.lat;
+        document.getElementById(`placeLng${placeBtnNum}`).value = data.lng;
+        document.getElementById(`place${placeBtnNum}`).value = data.name;
 
         if (data.budget)
-            document.getElementById(`budget${placeFormNum}`).value = data.budget;
+            document.getElementById(`budget${placeBtnNum}`).value = data.budget;
 
         if (data.stayTime)
-            document.getElementById(`stayTime${placeFormNum}`).value = data.stayTime;
+            document.getElementById(`stayTime${placeBtnNum}`).value = data.stayTime;
         else
-            document.getElementById(`stayTime${placeFormNum}`).value = 30;
+            document.getElementById(`stayTime${placeBtnNum}`).value = 30;
 
         if (data.desiredStartTime)
-            document.getElementById(`desiredStartTime${placeFormNum}`).value = data.desiredStartTime;
+            document.getElementById(`desiredStartTime${placeBtnNum}`).value = data.desiredStartTime;
 
         if (data.desiredEndTime)
-            document.getElementById(`desiredEndTime${placeFormNum}`).value = data.desiredEndTime;
+            document.getElementById(`desiredEndTime${placeBtnNum}`).value = data.desiredEndTime;
     }
 }
 
