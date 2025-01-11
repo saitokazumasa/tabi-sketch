476c0f038c35c077f1e8967e78eff010ccb5a7ff
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2025-01-11 17:25:55 +0900

    Session関連の処理を削除

--- src/main/resources/static/js/modal.js
+++ src/main/resources/static/js/modal.js
@@ -10,158 +10,13 @@ class PlaceNum {
     }
 }
 
-class SessionStorageList {
-    #startPlaceList = new Array(0);
-    #endPlaceList = new Array(0);
-    #placesList = new Array(0);
-
-    constructor() {
-        // Sessionがあった時に配列に入れる
-        if (this.getStartData()) this.#startPlaceList = this.getStartData();
-        if (this.getEndData()) this.#endPlaceList = this.getEndData();
-        if (!this.getPlacesList()) return;
-
-        // sessionから取得
-        this.#placesList = this.getPlacesList();
-        // sessionのnull|undefinedを除外
-        this.#placesList = this.#placesList.filter(place => place !== null && place !== undefined);
-        // 再保存して整合性を保つ
-        sessionStorage.setItem('place', JSON.stringify(this.#placesList));
-    }
-
-    /**
-     * 出発地点の情報をsessionに登録
-     */
-    setStartPlace() {
-        this.#startPlaceList = [];
-        this.#startPlaceList.push({
-            placeId: document.getElementById('startPlaceId').value,
-            lat: document.getElementById('startLat').value,
-            lng: document.getElementById('startLng').value,
-            name: document.getElementById('startPlace').value,
-            startTime: document.getElementById('startTime').value
-        });
-
-        sessionStorage.setItem('startPlace', JSON.stringify(this.#startPlaceList));
-    }
-
-    /**
-     * 終了地点の情報をsessionに登録
-     */
-    setEndPlace() {
-        this.#endPlaceList = [];
-        this.#endPlaceList.push({
-            placeId: document.getElementById('endPlaceId').value,
-            lat: document.getElementById('endLat').value,
-            lng: document.getElementById('endLng').value,
-            name: document.getElementById('endPlace').value
-        });
-
-        sessionStorage.setItem('endPlace', JSON.stringify(this.#endPlaceList));
-    }
-
-    /**
-     * 目的地の情報をsessionに登録
-     * @param formNum formの項番
-     */
-    setPlaces(formNum) {
-        this.#placesList[formNum-1] = {
-            placeId: document.getElementById(`placeId${formNum}`).value,
-            lat: document.getElementById(`placeLat${formNum}`).value,
-            lng: document.getElementById(`placeLng${formNum}`).value,
-            name: document.getElementById(`place${formNum}`).value,
-            budget: document.getElementById(`budget${formNum}`).value,
-            stayTime: document.getElementById(`stayTime${formNum}`).value,
-            desiredStartTime: document.getElementById(`desiredStartTime${formNum}`).value,
-            desiredEndTime: document.getElementById(`desiredEndTime${formNum}`).value,
-        };
-
-        sessionStorage.setItem('place', JSON.stringify(this.#placesList));
-    }
-
-    /**
-     * オススメ目的地をsessionStorage placeに登録
-     * @param formNum formの項番
-     * @param num オススメ目的地の識別番号
-     */
-    setRecommendPlace(formNum, num) {
-        this.#placesList[formNum-1] = {
-            placeId: document.getElementById(`recommendPlaceId${num}`).value,
-            lat: document.getElementById(`recommendLat${num}`).value,
-            lng: document.getElementById(`recommendLng${num}`).value,
-            name: document.getElementById(`recommend${num}`).value,
-            budget: document.getElementById(`recommendBudget${num}`).value,
-            stayTime: document.getElementById(`recommendStayTime${num}`).value,
-            desiredStartTime: document.getElementById(`recommendDesiredStartTime${num}`).value,
-            desiredEndTime: document.getElementById(`recommendDesiredEndTime${num}`).value,
-        };
-
-        sessionStorage.setItem('place', JSON.stringify(this.#placesList));
-    }
-
-    /**
-     * 出発地点をsessionから取得
-     * @returns {*}
-     */
-    getStartData() {
-        const startPlaceData = sessionStorage.getItem('startPlace');
-
-        if (!startPlaceData) return;
-        const startPlaceList = JSON.parse(startPlaceData);
-        return startPlaceList[0];
-    }
-
-    /**
-     * 終了地点をsessionから取得
-     * @returns {*}
-     */
-    getEndData() {
-        const endPlaceData = sessionStorage.getItem('endPlace');
-
-        if (!endPlaceData) return;
-        const endPlaceList = JSON.parse(endPlaceData);
-        return endPlaceList[0];
-    }
-
-    /**
-     * 目的地をsessionから取得
-     * @param num session.placeの項番
-     * @returns {*}
-     */
-    getPlacesData(num) {
-        const placeData = sessionStorage.getItem('place');
-
-        if (!placeData) return;
-        const placeList = JSON.parse(placeData);
-        return placeList[num];
-    }
-
-    /**
-     * 目的地のsession配列を取得
-     * @returns {any}
-     */
-    getPlacesList() {
-        const placesList = sessionStorage.getItem('place');
-
-        if (!placesList) return;
-        return JSON.parse(placesList);
-    }
-
-    /**
-     * 目的地sessionの一部削除
-     * @param num 削除したい項番（formNum-1）
-     */
-    deletePlace(num) {
-        this.#placesList[num] = null;
-        sessionStorage.setItem('place', JSON.stringify(this.#placesList));
-    }
-}
-
 class Fragment {
-    #value;
+    #toggle;
+    #form;
 
     constructor() {
-        this.#value = null;
+        this.#toggle = null;
+        this.#form = null;
     }
 
     /**
@@ -170,9 +25,16 @@ class Fragment {
      */
     async initialize() {
         try {
-            const response = await fetch(`/fragment/modal/places?num=${(placeNum.value()+1)}`);
+            const response = await fetch(`/fragment/modal/placesToggle?num=${(placeNum.value()+1)}`);
             if (!response.ok) { throw new Error('フラグメントの取得に失敗しました'); }
-            this.#value = await response.text();
+            this.#toggle = await response.text();
+        } catch (error) {
+            throw new Error('initialize Error : ' + error);
+        }
+        try {
+            const response = await fetch(`fragment/modal/placesForm?num=${(placeNum.value()+1)}`);
+            if (!response.ok) { throw new Error('フラグメントの取得に失敗しました'); }
+            this.#form = await response.text();
         } catch (error) {
             throw new Error('initialize Error : ' + error);
         }
@@ -182,21 +44,36 @@ class Fragment {
      * HTMLにfragment追加
      */
     addFragment() {
-        if (this.#value === null) throw new Error('このインスタンスは初期化されていません。initialize()を実行してください。');
-        // id=destination の子要素に追加
+        if (this.#toggle === null) throw new Error('このインスタンスは初期化されていません。initialize()を実行してください。');
+        // id=destination の子要素にToggleを追加
         const container = document.getElementById('destination');
-        const item = document.createElement('div');
-        item.innerHTML = this.#value;
-        container.appendChild(item);
+        const newToggle = document.createElement('div');
+        newToggle.innerHTML = this.#toggle;
+        container.appendChild(newToggle);
+        // mainタグの子要素に Form を追加
+        const main = document.querySelector('main');
+        const newForm = document.createElement('div');
+        newForm.innerHTML = this.#form;
+        main.appendChild(newForm);
+
     }
 
     /**
-     * 追加するfragment取得
+     * 追加する Toggle fragment取得
      * @returns {*}
      */
-    value() {
-        if (this.#value === null) throw new Error('このインスタンスは初期化されていません。initialize()を実行してください。');
-        return this.#value;
+    toggle() {
+        if (this.#toggle === null) throw new Error('このインスタンスは初期化されていません。initialize()を実行してください。');
+        return this.#toggle;
+    }
+
+    /**
+     * 追加する Form fragment取得
+     * @returns {*}
+     */
+    form() {
+        if (this.#form === null) throw new Error('このインスタンスは初期化されていません。initialize()を実行してください。');
+        return this.#form;
     }
 }
 
@@ -314,9 +191,9 @@ class ModalElement {
     changeStartDisplay() {
         const spans = document.querySelectorAll('#startToggle span'); // spanタグ取得
 
-        const data = sessionStorageList.getStartData();
-        spans[0].textContent = data.startTime; // 開始時間を入れる
-        spans[1].textContent = data.name; // spanの文字を場所名に
+        // todo: 表示変更する値を取得 data
+        // spans[0].textContent = data.startTime; // 開始時間を入れる
+        // spans[1].textContent = data.name; // spanの文字を場所名に
         spans[0].classList.remove('absolute');
     }
 
@@ -326,7 +203,8 @@ class ModalElement {
     changeEndDisplay() {
         const span = document.querySelector('#endToggle span'); // spanタグ取得
 
-        span.textContent = sessionStorageList.getEndData().name; // spanの文字を場所名に
+        // todo: 表示変更するspanの文字を変えて
+        // span.textContent = sessionStorageList.getEndData().name; // spanの文字を場所名に
     }
 
     /**
@@ -341,25 +219,27 @@ class ModalElement {
         placeInput.disabled = true; // 目的地部分をdisabledに
         placeInput.classList.add('bg-gray-100');
 
-        const data = sessionStorageList.getPlacesData(formNum-1);
+        // todo: 表示変更する値を取得 data
 
-        // 場所名
-        spans[1].textContent = data.name;
+        // const data = sessionStorageList.getPlacesData(formNum-1);
 
-        // 希望時間
-        if (!data.desiredStartTime) spans[0].textContent = '';
-        else spans[0].textContent = data.desiredStartTime + '~' + data.desiredEndTime;
-        spans[0].classList.remove('absolute');
-
-        // 予算
-        const budget = document.getElementById(`budgetDisplay${formNum}`);
-        if (!data.budget) budget.textContent = '予算：----' + '円';
-        else budget.textContent = '予算：' + data.budget + '円';
-
-        // 滞在時間
-        const stayTime = document.getElementById(`stayTimeDisplay${formNum}`);
-        if (!data.stayTime) stayTime.textContent = '滞在時間：30分';
-        else stayTime.textContent = '滞在時間：' + data.stayTime + '分';
+        // // 場所名
+        // spans[1].textContent = data.name;
+        //
+        // // 希望時間
+        // if (!data.desiredStartTime) spans[0].textContent = '';
+        // else spans[0].textContent = data.desiredStartTime + '~' + data.desiredEndTime;
+        // spans[0].classList.remove('absolute');
+        //
+        // // 予算
+        // const budget = document.getElementById(`budgetDisplay${formNum}`);
+        // if (!data.budget) budget.textContent = '予算：----' + '円';
+        // else budget.textContent = '予算：' + data.budget + '円';
+        //
+        // // 滞在時間
+        // const stayTime = document.getElementById(`stayTimeDisplay${formNum}`);
+        // if (!data.stayTime) stayTime.textContent = '滞在時間：30分';
+        // else stayTime.textContent = '滞在時間：' + data.stayTime + '分';
 
         // 緑色の枠線をけす
         const toggleBtn = this.#getToggleBtn('places', formNum-1);
@@ -380,9 +260,6 @@ class ModalElement {
             const toggleBtn = this.#getToggleBtn('places', formNum-1);
             toggleBtn.classList.add('hidden'); // Modal表示のボタンを隠す
             deleteBtn.classList.add('hidden'); // ✕ボタンを隠す
-
-            // sessionから消す
-            sessionStorageList.deletePlace(formNum-1);
         });
     }
 }
@@ -458,8 +335,6 @@ class ModalSubmitButton {
             return;
         }
 
-        sessionStorageList.setEndPlace();
-
         const modalType = 'end';
         modal.closeModal(modalType, 0);
         modal.addButtonEvent(modalType, 0);
@@ -497,8 +372,6 @@ class ModalSubmitButton {
             return;
         }
 
-        sessionStorageList.setPlaces(formNum);
-
         // modal設定
         const modalType = 'places';
         modal.closeModal(modalType, formNum-1);
@@ -531,7 +404,7 @@ class ModalSubmitButton {
         const newFragment = new Fragment();
         await newFragment.initialize();
 
-        if (!newFragment.value()) return; // 取得できなかったとき
+        if (!newFragment.toggle() && !newFragment.form()) return; // 取得できなかったとき
 
         newFragment.addFragment();
         placeNum.increment();
@@ -541,122 +414,10 @@ class ModalSubmitButton {
     }
 }
 
-/**
- * sessionに値がある時の初期設定用Class
- */
-class InitSessionModals {
-    #startData;
-    #endData;
-    #placesData;
-
-    constructor() {
-        this.#startData = sessionStorageList.getStartData();
-        this.#endData = sessionStorageList.getEndData();
-        this.#placesData = sessionStorageList.getPlacesList();
-    }
-
-    async initialize() {
-        // 開始地点がある場合
-        if (this.#startData) {
-            modal.changeStartDisplay();
-            this.#setStartFormValue();
-        }
-
-        // 終了地点がある時
-        if (this.#endData) {
-            modal.changeEndDisplay();
-            this.#setEndFormValue();
-        }
-
-        // 目的地がある時
-        if (this.#placesData && this.#placesData.length > 0) {
-            await this.#initializePlaces();
-        }
-    }
-
-    /**
-     * 出発地点inputにsessionの値をvalueで挿入
-     */
-    #setStartFormValue() {
-        document.getElementById('startPlaceId').value = this.#startData.placeId;
-        document.getElementById('startLat').value = this.#startData.lat;
-        document.getElementById('startLng').value = this.#startData.lng;
-        document.getElementById('startPlace').value = this.#startData.name;
-        document.getElementById('startTime').value = this.#startData.startTime;
-    }
-
-    /**
-     * 終了地点inputにsessionの値をvalueで挿入
-     */
-    #setEndFormValue() {
-        document.getElementById('endPlaceId').value = this.#endData.placeId;
-        document.getElementById('endLat').value = this.#endData.lat;
-        document.getElementById('endLng').value = this.#endData.lng;
-        document.getElementById('endPlace').value = this.#endData.name;
-    }
-
-    /**
-     * 目的地inputにsessionの値をvalueで挿入
-     * @param formNum formIDの項番
-     * @param num session.placeの項番
-     */
-    #setPlaceFormValue(formNum ,num) {
-        document.getElementById(`placeId${formNum}`).value = this.#placesData[num].placeId;
-        document.getElementById(`placeLat${formNum}`).value = this.#placesData[num].lat;
-        document.getElementById(`placeLng${formNum}`).value = this.#placesData[num].lng;
-        document.getElementById(`place${formNum}`).value = this.#placesData[num].name;
-
-        if (this.#placesData[num].budget)
-            document.getElementById(`budget${formNum}`).value = this.#placesData[num].budget;
-
-        if (this.#placesData[num].stayTime)
-            document.getElementById(`stayTime${formNum}`).value = this.#placesData[num].stayTime;
-        else
-            document.getElementById(`stayTime${formNum}`).value = 30;
-
-        if (this.#placesData[num].desiredStartTime)
-            document.getElementById(`desiredStartTime${formNum}`).value = this.#placesData[num].desiredStartTime;
-
-        if (this.#placesData[num].desiredEndTime)
-            document.getElementById(`desiredEndTime${formNum}`).value = this.#placesData[num].desiredEndTime;
-    }
-
-    /**
-     * 目的地の設定
-     * inputにvalue
-     * fragment追加
-     * placeNum合わせる
-     * @returns {Promise<void>}
-     */
-    async #initializePlaces() {
-        // 無効なデータ（nullやundefined）を除外
-        this.#placesData = this.#placesData.filter(place => place !== null && place !== undefined);
-        for (let i = 0; i < this.#placesData.length; i++) {
-            modal.changePlaceDisplay(i+1);
-            this.#setPlaceFormValue(i+1, i);
-
-            // 新規フラグメント呼び出し
-            const newFragment = new Fragment();
-            await newFragment.initialize();
-
-            if (!newFragment.value()) return;
-
-            newFragment.addFragment();
-            if (i !== 0) modal.addButtonEvent('places', i);
-            placeNum.increment();
-            modal.addPlacesElement();
-            if (i !== 0 && i === this.#placesData.length-1) modal.addButtonEvent('places', i+1);
-        }
-    }
-}
-
 const placeNum = new PlaceNum();
-const sessionStorageList = new SessionStorageList();
 const modal = new ModalElement();
 
 async function initModal() {
-    const initializeDisplay = new InitSessionModals();
-    await initializeDisplay.initialize();
     new ModalSubmitButton();
 }
 
