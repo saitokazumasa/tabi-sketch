d06ed282e3d2afab6ec315066405ad06bcc83119
saito-kazumasa <kotobukisyo@gmail.com>
2025-01-15 12:43:46 +0900

    GooglePlaceMapperにselectを追加

--- src/test/java/com/tabisketch/mapper/GooglePlaceMapperTest.java
+++ src/test/java/com/tabisketch/mapper/GooglePlaceMapperTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.test.context.jdbc.Sql;
 
 @MybatisTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -22,5 +23,15 @@ public class GooglePlaceMapperTest {
         );
         assert this.googlePlaceMapper.insert(googlePlace) == 1;
         assert googlePlace.getId() != -1;
+    }
+
+    @Test
+    @Sql({
+            "classpath:/sql/CreateGooglePlace.sql"
+    })
+    public void testSelect() {
+        final var placeId = "";
+        final var googlePlace = this.googlePlaceMapper.selectByPlaceId(placeId);
+        assert googlePlace != null;
     }
 }
