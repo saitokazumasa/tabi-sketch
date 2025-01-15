147fe9bf4588f5d3e6c94c294fcceb7b0a08bda3
saito-kazumasa <kotobukisyo@gmail.com>
2025-01-15 12:56:14 +0900

    CreatePlaceServiceとCreateGooglePlaceServiceを結合

--- src/test/java/com/tabisketch/service/CreatePlaceServiceTest.java
+++ src/test/java/com/tabisketch/service/CreatePlaceServiceTest.java
@@ -2,6 +2,7 @@ package com.tabisketch.service;
 
 import com.tabisketch.bean.form.CreatePlaceForm;
 import com.tabisketch.exception.InsertFailedException;
+import com.tabisketch.mapper.IGooglePlaceMapper;
 import com.tabisketch.mapper.IPlacesMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,8 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import java.time.LocalTime;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -19,13 +22,20 @@ public class CreatePlaceServiceTest {
     private ICreatePlaceService createPlaceService;
     @MockitoBean
     private IPlacesMapper placesMapper;
+    @MockitoBean
+    private IGooglePlaceMapper googlePlaceMapper;
 
     @Test
     public void testExecute() throws InsertFailedException {
+        when(this.googlePlaceMapper.selectByPlaceId(anyString())).thenReturn(null);
+        when(this.googlePlaceMapper.insert(any())).thenReturn(1);
         when(this.placesMapper.insert(any())).thenReturn(1);
 
         final var createPlaceForm = new CreatePlaceForm(
-                1,
+                "",
+                "",
+                0,
+                0,
                 1,
                 0,
                 LocalTime.of(10, 0),
@@ -38,5 +48,9 @@ public class CreatePlaceServiceTest {
         );
 
         this.createPlaceService.execute(createPlaceForm);
+
+        verify(this.googlePlaceMapper).selectByPlaceId(anyString());
+        verify(this.googlePlaceMapper).insert(any());
+        verify(this.placesMapper).insert(any());
     }
 }
