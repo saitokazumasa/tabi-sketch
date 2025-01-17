f31ea23131ec4948661d5762811b2a4189d004dc
saito-kazumasa <kotobukisyo@gmail.com>
2025-01-17 09:52:39 +0900

    tailwindcssをビルドするシェルスクリプト内でのパス区切りをフォワードスラッシュに変更

--- bin/BuildTailwindCss.sh
+++ bin/BuildTailwindCss.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Set env
-INPUT_CSS_PATH=".\src\main\resources\static\css\input.css"
-OUTPUT_CSS_PATH=".\src\main\resources\static\css\tailwind.css"
+INPUT_CSS_PATH="./src/main/resources/static/css/input.css"
+OUTPUT_CSS_PATH="./src/main/resources/static/css/tailwind.css"
 
 # Run
 npx tailwindcss -i $INPUT_CSS_PATH -o $OUTPUT_CSS_PATH
