222ac30b60e9a93120fae7a8f3a2ec2c3114a3bc
saitokazumasa <kotobukisyo@gmail.com>
2025-01-20 14:03:00 +0900

    CreateTables.shのパスを修正

--- bin/CreateTables.sh
+++ bin/CreateTables.sh
@@ -11,14 +11,14 @@ PGPASSWORD="$POSTGRES_PASSWORD"
 set +o allexport
 
 # Run
-psql -f ./docker/postgresql/sql/01-create-users-table.sql
-psql -f ./docker/postgresql/sql/02-create-password-reset-tokens-table.sql
-psql -f ./docker/postgresql/sql/03-create-maa-tokens-table.sql
-psql -f ./docker/postgresql/sql/04-create-plans-table.sql
-psql -f ./docker/postgresql/sql/05-create-days-table.sql
-psql -f ./docker/postgresql/sql/06-create-google-places-table.sql
-psql -f ./docker/postgresql/sql/07-create-google-place-types-table.sql
-psql -f ./docker/postgresql/sql/08-create-google-type-associations-table.sql
-psql -f ./docker/postgresql/sql/09-create-transportations-type.sql
-psql -f ./docker/postgresql/sql/10-create-places-table.sql
-psql -f ./docker/postgresql/sql/11-create-packings-table.sql
+psql -f docker/postgresql/sql/01-create-users-table.sql
+psql -f docker/postgresql/sql/02-create-password-reset-tokens-table.sql
+psql -f docker/postgresql/sql/03-create-maa-tokens-table.sql
+psql -f docker/postgresql/sql/04-create-plans-table.sql
+psql -f docker/postgresql/sql/05-create-days-table.sql
+psql -f docker/postgresql/sql/06-create-google-places-table.sql
+psql -f docker/postgresql/sql/07-create-google-place-types-table.sql
+psql -f docker/postgresql/sql/08-create-google-type-associations-table.sql
+psql -f docker/postgresql/sql/09-create-transportations-type.sql
+psql -f docker/postgresql/sql/10-create-places-table.sql
+psql -f docker/postgresql/sql/11-create-packings-table.sql
