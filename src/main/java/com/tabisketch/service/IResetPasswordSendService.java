ca07967361aeddf637ada65bb9ef2f9578dc284a
ken-250 <k.kawaguchi.sys21@morijyobi.ac.jp>
2025-01-20 23:29:38 +0900

    リファクタ

--- src/main/java/com/tabisketch/service/IResetPasswordSendService.java
+++ src/main/java/com/tabisketch/service/IResetPasswordSendService.java
@@ -3,6 +3,6 @@ package com.tabisketch.service;
 import com.tabisketch.exception.InsertFailedException;
 import jakarta.mail.MessagingException;
 
-public interface ISendPasswordResetMailService {
+public interface IResetPasswordSendService {
     void execute(final String mailAddress) throws InsertFailedException, MessagingException;
 }
