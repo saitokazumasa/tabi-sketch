32982e540cf470e0ade215331fc1ea5bc7866565
ken-250 <k.kawaguchi.sys21@morijyobi.ac.jp>
2025-01-20 14:17:06 +0900

    パスワード変更画面で使用するformを作成

---
+++ src/main/java/com/tabisketch/bean/form/ResetPasswordForm.java
@@ -0,0 +1,29 @@
+package com.tabisketch.bean.form;
+
+import com.tabisketch.util.StringUtils;
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ResetPasswordForm {
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private String rePassword;
+
+    public static ResetPasswordForm empty() {
+        return new ResetPasswordForm("", "");
+    }
+
+    @AssertTrue(message = "パスワードが一致しません")
+    public boolean isMatchPasswordAndRePassword() {
+        if (StringUtils.nullAndEmpty(this.password)) return false;
+        if (StringUtils.nullAndEmpty(this.rePassword)) return false;
+
+        return password.equals(rePassword);
+    }
+}
