526d176741aed570773ece6db76692171aa52a3f
ken-250 <k.kawaguchi.sys21@morijyobi.ac.jp>
2025-01-20 14:16:49 +0900

    select文を追加

--- src/main/java/com/tabisketch/mapper/IPasswordResetTokensMapper.java
+++ src/main/java/com/tabisketch/mapper/IPasswordResetTokensMapper.java
@@ -8,4 +8,7 @@ public interface IPasswordResetTokensMapper {
     @Insert("INSERT INTO password_reset_tokens (token, user_id) VALUES (#{token}, #{userId})")
     @Options(useGeneratedKeys = true, keyProperty = "id")
     int insert(final PasswordResetToken passwordResetToken);
+
+    @Select("SELECT * FROM password_reset_tokens WHERE token = #{token}")
+    PasswordResetToken selectByToken(final String token);
 }
