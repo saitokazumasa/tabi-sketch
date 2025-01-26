f37a3e80eca015ec4e17d1a6ad92e083d9ed8486
Saito Kazumasa <kotobukisyo@gmail.com>
2025-01-26 22:44:07 +0900

    ExampleResetPasswordFormを修正

--- src/test/java/com/tabisketch/bean/form/ExampleResetPasswordForm.java
+++ src/test/java/com/tabisketch/bean/form/ExampleResetPasswordForm.java
@@ -3,6 +3,7 @@ package com.tabisketch.bean.form;
 public class ExampleResetPasswordForm {
     public static ResetPasswordForm generate() {
         return new ResetPasswordForm(
+                "d3a460f2-5df4-48b6-b9e1-a550e319512f",
                 "password",
                 "password"
         );
