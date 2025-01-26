a69459f49a4534e8434bf961f435cf1a7b3bef68
Saito Kazumasa <kotobukisyo@gmail.com>
2025-01-26 22:45:45 +0900

    ExamplePasswordResetTokenを修正

--- src/test/java/com/tabisketch/bean/entity/ExamplePasswordResetToken.java
+++ src/test/java/com/tabisketch/bean/entity/ExamplePasswordResetToken.java
@@ -5,8 +5,7 @@ import java.util.UUID;
 
 public class ExamplePasswordResetToken {
     public static PasswordResetToken generate() {
-        final UUID token = UUID.fromString("d3a460f2-5df4-48b6-b9e1-a550e319512f");
-
+        final var token = UUID.fromString("d3a460f2-5df4-48b6-b9e1-a550e319512f");
         return new PasswordResetToken(
                 1,
                 token,
