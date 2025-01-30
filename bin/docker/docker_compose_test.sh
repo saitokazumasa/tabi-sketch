f77f354df129946bde56d5313c6c02d34d2e8a0e
saitokazumasa <kotobukisyo@gmail.com>
2025-01-30 10:12:17 +0900

    docker-composeファイルが増えるたびに、package.jsonに追記するのが煩わしい&長くなってしまうため、shellで自動化した

---
+++ bin/docker/docker_compose_test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Get all docker-compose.yml files
+DOCKER_DIRECTORY=$1
+DOCKER_COMPOSE_LIST=$(find "$DOCKER_DIRECTORY" -type f -name "*docker-compose*.yml")
+
+# Build docker command
+DOCKER_COMMAND="docker compose "
+for DOCKER_COMPOSE in $DOCKER_COMPOSE_LIST; do
+  DOCKER_COMMAND+="-f $DOCKER_COMPOSE "
+done
+DOCKER_COMMAND+="up test"
+
+# Run
+echo "run \"$DOCKER_COMMAND\""
+$DOCKER_COMMAND
--- bin/docker_compose_build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker_directory=$1
-docker_compose_list=$(find $docker_directory -name "*docker_compose*.yml")
-
