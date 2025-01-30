e7db523ff51c9b4ae1aa8ecc629db352d3685430
saitokazumasa <kotobukisyo@gmail.com>
2025-01-30 13:25:23 +0900

    DB初期化シェルで、ファイルを自動で取得して実行させる方式だと、順番が意図したとおりにならず失敗する

--- bin/github_workflows/init_db.sh
+++ bin/github_workflows/init_db.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Get all .sql files
-SQL_DIRECTORY="docker/postgresql/sql"
-SQL_LIST=$(find "$SQL_DIRECTORY" -type f -name "*.sql")
-
 # Set env
 set -o allexport
 PGHOST="$POSTGRES_HOST"
@@ -14,20 +10,14 @@ PGPASSWORD="$POSTGRES_PASSWORD"
 set +o allexport
 
 # Run
-for SQL in $SQL_LIST; do
-  psql -f "$SQL"
-done
-
-echo "Sql list: $SQL_LIST"
-
-#psql -f docker/postgresql/sql/01-create-users-table.sql
-#psql -f docker/postgresql/sql/02-create-password-reset-tokens-table.sql
-#psql -f docker/postgresql/sql/03-create-maa-tokens-table.sql
-#psql -f docker/postgresql/sql/04-create-plans-table.sql
-#psql -f docker/postgresql/sql/05-create-days-table.sql
-#psql -f docker/postgresql/sql/06-create-google-places-table.sql
-#psql -f docker/postgresql/sql/07-create-google-place-types-table.sql
-#psql -f docker/postgresql/sql/08-create-google-type-associations-table.sql
-#psql -f docker/postgresql/sql/09-create-transportations-type.sql
-#psql -f docker/postgresql/sql/10-create-places-table.sql
-#psql -f docker/postgresql/sql/11-create-packings-table.sql
+psql -f docker/postgresql/sql/01-create-users-table.sql
+psql -f docker/postgresql/sql/02-create-password-reset-tokens-table.sql
+psql -f docker/postgresql/sql/03-create-maa-tokens-table.sql
+psql -f docker/postgresql/sql/04-create-plans-table.sql
+psql -f docker/postgresql/sql/05-create-days-table.sql
+psql -f docker/postgresql/sql/06-create-google-places-table.sql
+psql -f docker/postgresql/sql/07-create-google-place-types-table.sql
+psql -f docker/postgresql/sql/08-create-google-type-associations-table.sql
+psql -f docker/postgresql/sql/09-create-transportations-type.sql
+psql -f docker/postgresql/sql/10-create-places-table.sql
+psql -f docker/postgresql/sql/11-create-packings-table.sql
