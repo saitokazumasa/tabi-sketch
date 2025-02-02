b9c101535672313848dea05d85bacc25c069d98f
saitokazumasa <saitokazumasa.jp@gmail.com>
2025-02-02 23:48:47 +0900

    一旦UsersMapperの不要な部分を削除

--- src/main/java/com/tabisketch/mapper/IUsersMapper.java
+++ src/main/java/com/tabisketch/mapper/IUsersMapper.java
@@ -1,39 +1,10 @@
-//package com.tabisketch.mapper;
-//
-//import com.tabisketch.bean.entity.User;
-//import org.apache.ibatis.annotations.*;
-//
-//@Mapper
-//public interface IUsersMapper {
-//    @Insert("INSERT INTO users (mail_address, password) VALUES (#{mailAddress}, #{password})")
-//    @Options(useGeneratedKeys = true, keyProperty = "id")
-//    int insert(final User user);
-//
-//    @Select("SELECT * FROM users WHERE id = #{id}")
-//    User selectById(final int id);
-//
-//    @Select("SELECT * FROM users WHERE mail_address = #{mailAddress}")
-//    User selectByMailAddress(final String mailAddress);
-//
-//    @Update("UPDATE users " +
-//            "   SET mail_address = #{mailAddress}, " +
-//            "       password = #{password}, " +
-//            "       mail_address_authenticated = #{mailAddressAuthenticated} " +
-//            "   WHERE id = #{id}")
-//    int update(final User user);
-//
-//    @Update("UPDATE users " +
-//            "   SET mail_address = #{mailAddress} " +
-//            "   WHERE id = #{id}")
-//    int updateMailAddress(final int id, final String mailAddress);
-//
-//    @Update("UPDATE users " +
-//            "   SET password = #{password} " +
-//            "   WHERE id = #{id}")
-//    int updatePassword(final int id, final String password);
-//
-//    @Update("UPDATE users " +
-//            "   SET mail_address_authenticated = #{mailAddressAuthenticated} " +
-//            "   WHERE id = #{id}")
-//    int updateMailAddressAuthenticated(final int id, final boolean mailAddressAuthenticated);
-//}
+package com.tabisketch.mapper;
+
+import com.tabisketch.bean.entity.User;
+import org.apache.ibatis.annotations.*;
+
+@Mapper
+public interface IUsersMapper {
+    @Select("SELECT * FROM users WHERE email = #{email}")
+    User selectByEmail(final String email);
+}
