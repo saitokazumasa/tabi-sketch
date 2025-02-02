557abe4eb5b0bb994ff53d7a6783461a287158c4
saitokazumasa <saitokazumasa.jp@gmail.com>
2025-02-02 19:27:09 +0900

    IndexControllerをリファクタ

--- src/main/java/com/tabisketch/controller/IndexController.java
+++ src/main/java/com/tabisketch/controller/IndexController.java
@@ -1,14 +1,19 @@
-//package com.tabisketch.controller;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//@Controller
-//@RequestMapping("/")
-//public class IndexController {
-//    @GetMapping
-//    public String get() {
-//        return "redirect:/top";
-//    }
-//}
+package com.tabisketch.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class IndexController {
+    @GetMapping("/")
+    public String index() {
+        return "top";
+    }
+
+    @GetMapping("/top")
+    public String top() {
+        return "top";
+    }
+}
